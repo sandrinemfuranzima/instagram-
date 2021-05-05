ae798a9a9d490c00f1fb0969f860d4d9fbf15673
sandrinemfuranzima <75666150+sandrinemfuranzima@users.noreply.github.com>
2021-05-05 12:29:03 +0200

    Update forms.py

--- inst/forms.py
+++ inst/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from .models import Comment,Profile,Pic
 from django.contrib.auth.forms import AuthenticationForm
 
+
 class ProfileForm(forms.ModelForm):
 	model = Profile
 	username = forms.CharField(label='Username',max_length = 30)
